b3a0e4106cf0d063e6d99a024bdb3ef719d7c3f9
Juan Chalita <chalajr2@gmail.com>
2021-10-19 18:05:56 -0500

    FInished section 6 chapter 43

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100.0,
+                color: Colors.red,
+                child: Text('Container 1'),
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 100.0,
+                color: Colors.blue,
+                child: Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
